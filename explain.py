fcd047e332a38ff0b914617abfc5fd5ab4850637
SHAHNAB AHMED <31092371+Shahnab@users.noreply.github.com>
2022-09-15 10:24:21 +0700

    Update explain.py

--- explain.py
+++ explain.py
@@ -14,7 +14,7 @@ import lime.lime_tabular
 import os
 import streamlit as st
 
-
+st.image('https://assets-eu-01.kc-usercontent.com/3704723c-bfe7-01fd-0ea6-811f709c0da5/fbaba2e2-fbbe-41b8-a78e-5e42e01dbbe1/Dentsu%20Logo_White.png', width=250)
 def perm_import(
     model,
     X_val,
